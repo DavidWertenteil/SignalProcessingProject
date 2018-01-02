6daca69d2f365d7fcf2f614cee0e78a7a5075ae2
David Wertenteil <davhadassah@gmail.com>
2018-01-02 14:21:41 +0200

    Tree

--- utilities/convertNameActivity.m
+++ utilities/convertNameActivity.m
@@ -1,15 +1,31 @@
 function act_names = convertNameActivity(number)
-%UNTITLED2 Summary of this function goes here
-%   Detailed explanation goes here
-if(number == 7)
-    act_names = 2;
-elseif (number == 8)
-    act_names = 3;
-elseif (number == 12)
-    act_names = 4;
-else
-    act_names = 1;
+%
+%  The original:
+%      Walking (1)
+%      WalkingUpstairs (2)
+%      WalkingDownstairs (3)
+%      Sitting (4)
+%      Standing (5)
+%      Laying (6)
+%      DragLimp (7)
+%      JumpLimp (8)
+%      PersonFall (9)
+%      PhoneFall (10)
+%      Running(11)
+%      AustoLimp(12)
+%%
+%if(number == 7)
+%    act_names = 2;
+%elseif (number == 8)
+%    act_names = 3;
+%elseif (number == 12)
+%    act_names = 4;
+%else
+%    act_names = 1;
+%end
+act_names = number;
+if(number == 11 || number == 12)
+    act_names = number - 2;
 end
 
-end
 
